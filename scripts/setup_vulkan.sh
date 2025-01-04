e1a67a4f3014399cb510943115401c360673774b
Yauheni <yauhenbelarus@gmail.com>
2025-01-05 00:01:40 +0300

    hotfix setup

--- scripts/setup_vulkan.sh
+++ scripts/setup_vulkan.sh
@@ -1,3 +1,5 @@
+sudo -v
+
 wget https://sdk.lunarg.com/sdk/download/1.3.296.0/linux/vulkan-sdk.tar.gz -O vulkan-sdk.tar.gz
 sudo tar -xJf vulkan-sdk.tar.gz -C /opt/
 
@@ -10,6 +12,8 @@ echo 'export LD_LIBRARY_PATH=$VULKAN_SDK/lib:$LD_LIBRARY_PATH' >> ~/.bashrc
 echo 'export VK_ICD_FILENAMES=/usr/share/vulkan/icd.d/intel_icd.x86_64.json' >> ~/.bashrc
 # shellcheck disable=SC2016
 echo 'export VK_LAYER_PATH=$VULKAN_SDK/etc/vulkan/explicit_layer.d' >> ~/.bashrc
+
+rm vulkan-sdk.tar.gz
 
 # shellcheck disable=SC1090
 source ~/.bashrc
